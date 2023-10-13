276bdf6ff5ef21e2b18446820cee68844179f28f
Sameeran Amarnath <sameeranamarnath@gmail.com>
2023-10-13 11:01:18 +0530

    changes v1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // Base path to deploy to GitHub Pages
-  base: "/example-meeting-notes/",
+  base: "/meetinNotesTraacker/",
   plugins: [react()],
 });
